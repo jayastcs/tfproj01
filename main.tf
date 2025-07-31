5d4f939e0f2c9dd9a739126a386450960e3af409
jayastcs <jayasimha1.k@tcs.com>
2025-07-31 02:40:45 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,20 @@
+terraform {
 
+   required_version = ">=0.12"
+
+   required_providers {
+     azurerm = {
+       source = "hashicorp/azurerm"
+       version = "~>2.0"
+     }
+   }
+ }
+
+ provider "azurerm" {
+   features {}
+ }
+
+ resource "azurerm_resource_group" "rfspf" {
+   name     = "RG-Project"   #Name dispalyed in the Resource.. 
+   location = "West US 2"
+ }
