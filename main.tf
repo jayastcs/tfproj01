40851afcc47a97cf33a694d0cae78dd61a9d5cf7
jayastcs <jayasimha1.k@tcs.com>
2026-01-22 16:24:14 +0530

    Create main.tf file with Azure Terraform configuration

--- main.tf
+++ main.tf
@@ -1,22 +1,160 @@
-#added 1st line
-#another update from git
 terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
 
-   required_version = ">=0.12"
+provider "azurerm" {
+  features {}
+}
 
-   required_providers {
-     azurerm = {
-       source = "hashicorp/azurerm"
-       version = "~>2.0"
-     }
-   }
- }
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "westeurope"
+}
 
- provider "azurerm" {
-   features {}
- }
+variable "resource_group_name" {
+  description = "Resource group name"
+  type        = string
+  default     = "rg-linux-vm"
+}
 
- resource "azurerm_resource_group" "rfspf" {
-   name     = "RG-Project"   #Name dispalyed in the Resource.. 
-   location = "West US 2"
- }
+variable "vnet_name" {
+  type    = string
+  default = "vnet-linux-vm"
+}
+
+variable "subnet_name" {
+  type    = string
+  default = "subnet"
+}
+
+variable "address_space" {
+  type    = list(string)
+  default = ["10.0.0.0/16"]
+}
+
+variable "subnet_prefix" {
+  type    = string
+  default = "10.0.1.0/24"
+}
+
+variable "vm_name" {
+  type    = string
+  default = "linux-vm"
+}
+
+variable "vm_size" {
+  type    = string
+  default = "Standard_B1s"
+}
+
+variable "admin_username" {
+  type    = string
+  default = "azureuser"
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to the SSH public key to install (local file path)"
+  type        = string
+  default     = "~/.ssh/id_rsa.pub"
+}
+
+variable "tags" {
+  type = map(string)
+  default = {
+    created_by = "terraform"
+    env        = "dev"
+  }
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = var.vnet_name
+  address_space       = var.address_space
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = var.subnet_name
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = [var.subnet_prefix]
+}
+
+resource "azurerm_public_ip" "pubip" {
+  name                = "${var.vm_name}-pip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Dynamic"
+  sku                 = "Basic"
+  tags                = var.tags
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "${var.vm_name}-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "ipconfig1"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pubip.id
+  }
+
+  tags = var.tags
+}
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = var.vm_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  admin_ssh_key {
+    username   = var.admin_username
+    public_key = file(var.ssh_public_key_path)
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    name                 = "${var.vm_name}-osdisk"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+
+  tags = var.tags
+}
+
+output "public_ip" {
+  description = "Public IP address of the VM"
+  value       = azurerm_public_ip.pubip.ip_address
+}
+
+output "vm_id" {
+  description = "Azure resource id of the VM"
+  value       = azurerm_linux_virtual_machine.vm.id
+}
