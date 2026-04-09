7f2fa337f4e0831df412ff89f170326d29e7fd24
jayastcs <jayasimha1.k@tcs.com>
2026-04-09 13:41:55 +0530

    Migrate from Azure to AWS resources in main.tf

--- main.tf
+++ main.tf
@@ -1,160 +1,110 @@
-terraform {
-  required_version = ">= 1.0.0"
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = ">= 3.0.0"
-    }
+provider "aws" {
+  region = "us-east-1"
+}
+
+############################
+# VPC
+############################
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name = "US-east1-VPC"
   }
 }
 
-provider "azurerm" {
-  features {}
-}
+############################
+# Internet Gateway
+############################
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
 
-variable "location" {
-  description = "Azure region"
-  type        = string
-  default     = "westeurope"
-}
-
-variable "resource_group_name" {
-  description = "Resource group name"
-  type        = string
-  default     = "rg-linux-vm"
-}
-
-variable "vnet_name" {
-  type    = string
-  default = "vnet-linux-vm"
-}
-
-variable "subnet_name" {
-  type    = string
-  default = "subnet"
-}
-
-variable "address_space" {
-  type    = list(string)
-  default = ["10.0.0.0/16"]
-}
-
-variable "subnet_prefix" {
-  type    = string
-  default = "10.0.1.0/24"
-}
-
-variable "vm_name" {
-  type    = string
-  default = "linux-vm"
-}
-
-variable "vm_size" {
-  type    = string
-  default = "Standard_B1s"
-}
-
-variable "admin_username" {
-  type    = string
-  default = "azureuser"
-}
-
-variable "ssh_public_key_path" {
-  description = "Path to the SSH public key to install (local file path)"
-  type        = string
-  default     = "~/.ssh/id_rsa.pub"
-}
-
-variable "tags" {
-  type = map(string)
-  default = {
-    created_by = "terraform"
-    env        = "dev"
+  tags = {
+    Name = "Demo-IGW"
   }
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = var.resource_group_name
-  location = var.location
-  tags     = var.tags
+############################
+# Public Subnet 1
+############################
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "ap-south-1a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet-1"
+  }
 }
 
-resource "azurerm_virtual_network" "vnet" {
-  name                = var.vnet_name
-  address_space       = var.address_space
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tags                = var.tags
+############################
+# Public Subnet 2
+############################
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "ap-south-1b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "Public-Subnet-2"
+  }
 }
 
-resource "azurerm_subnet" "subnet" {
-  name                 = var.subnet_name
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = [var.subnet_prefix]
+############################
+# Private Subnet 1
+############################
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "Private-Subnet-1"
+  }
 }
 
-resource "azurerm_public_ip" "pubip" {
-  name                = "${var.vm_name}-pip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
-  sku                 = "Basic"
-  tags                = var.tags
+############################
+# Private Subnet 2
+############################
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+    Name = "Private-Subnet-2"
+  }
 }
 
-resource "azurerm_network_interface" "nic" {
-  name                = "${var.vm_name}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+############################
+# Public Route Table
+############################
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.main.id
 
-  ip_configuration {
-    name                          = "ipconfig1"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.pubip.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
   }
 
-  tags = var.tags
+  tags = {
+    Name = "Public-RouteTable"
+  }
 }
 
-resource "azurerm_linux_virtual_machine" "vm" {
-  name                = var.vm_name
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
-  size                = var.vm_size
-  admin_username      = var.admin_username
-  network_interface_ids = [
-    azurerm_network_interface.nic.id
-  ]
-
-  admin_ssh_key {
-    username   = var.admin_username
-    public_key = file(var.ssh_public_key_path)
-  }
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-    name                 = "${var.vm_name}-osdisk"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-
-  tags = var.tags
+############################
+# Route Table Association
+############################
+resource "aws_route_table_association" "public_assoc_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_rt.id
 }
 
-output "public_ip" {
-  description = "Public IP address of the VM"
-  value       = azurerm_public_ip.pubip.ip_address
-}
-
-output "vm_id" {
-  description = "Azure resource id of the VM"
-  value       = azurerm_linux_virtual_machine.vm.id
+resource "aws_route_table_association" "public_assoc_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_rt.id
 }
